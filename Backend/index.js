1f19f0355a59de13b95ba12369e3fae93730ae6b
Pravindu <pravindubh@gmail.com>
2024-01-30 15:06:36 +0530

    Database Connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,30 @@
 import express from "express";
-
+import mysql from "mysql";
 
 const app = express()
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Prabha@79641",
+    database:"book_mgt"
+})
+
+app.get("/",(req,res) => {
+    res.json("Hello I am backend")
+})
+
+app.get("/books",(req,res) => {
+    const que = "SELECT * FROM books"
+    db.query(que,(err,data)=>{
+        if(err){
+            return res.json(err)
+        } 
+        else{
+            return res.json(data)
+        }
+    })
+})
 
 app.listen(8800, () => {
     console.log("Connected to backend!");

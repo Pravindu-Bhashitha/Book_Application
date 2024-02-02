ba4e7db6bc7284fe2d21366ff8c494b5f268538d
Pravindu <pravindubh@gmail.com>
2024-02-02 15:33:59 +0530

    Delete API done

--- Backend/index.js
+++ Backend/index.js
@@ -45,6 +45,15 @@ app.post("/books",(req,res) => {
     })
 })
 
+app.delete("/books/:id",(req,res)=>{
+    const bookId = req.params.id;
+    const que = "DELETE FROM books WHERE id = ?"
+
+    db.query(q,[bookId],(err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Book has been deleted");
+    })
+})
 app.listen(8800, () => {
     console.log("Connected to backend!");
 })

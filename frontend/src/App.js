02764ada19812ec6f9a88a5e461c746e82d7ff4f
Pravindu <pravindubh@gmail.com>
2024-02-02 12:12:41 +0530

    Add the frontend & Done some navigations

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Books from './Pages/Books';
+import Add from './Pages/Add';
+import Update from './Pages/Update';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Books/>}/>
+        <Route path='/add' element={<Add/>}/>
+        <Route path='/update' element={<Update/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
